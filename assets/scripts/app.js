000ff52e5b310218b5a5c737f1942a00f816fc37
Reggie <reggieduran15@gmail.com>
2021-07-19 19:32:14 +0800

    Used dataset and addon sizes files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -35,21 +35,22 @@ class Component {
 }
 
 class Tooltip extends Component {
-    constructor(closeNotifierFunction) {
+    constructor(closeNotifierFunction, text) {
         super();
+        this.text = text;
         this.closeNotifierHandler = closeNotifierFunction;
         this.create();
     }
 
     closeTooltip = () => {
         this.detach();
-        this.closeNotifierFunction();
+        this.closeNotifierHandler();
     }
 
     create() {
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
-        tooltipElement.textContent = 'dummy';
+        tooltipElement.textContent = this.text;
         tooltipElement.addEventListener('click', this.closeTooltip);
         this.element = tooltipElement;
     }
@@ -69,9 +70,11 @@ class ProjectItem {
         if (this.hasActiveTooltip) {
             return;
         }
+        const projectElement = document.getElementById(this.id);
+        const tooltipText = projectElement.dataset.extraInfo;
         const tooltip = new Tooltip(() => {
             this.hasActiveTooltip = false;
-        });
+        }, tooltipText);
         tooltip.attach();
         this.hasActiveTooltip = true;
     }
@@ -79,7 +82,7 @@ class ProjectItem {
     connectMoreInfoButton() {
         const projectItemElement = document.getElementById(this.id);
         const moreInfoBtn = projectItemElement.querySelector('button:first-of-type');
-        moreInfoBtn.addEventListener('click', this.showMoreInfoHandler);
+        moreInfoBtn.addEventListener('click', this.showMoreInfoHandler.bind(this));
     }
 
     connectSwitchButton(type) {
